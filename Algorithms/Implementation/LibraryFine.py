efbbdb3d7cd93791c1b9c7c2149cc0b3fa91fcc5
LJLee37 <ljlee3759@gmail.com>
2020-11-03 22:33:07 +0900

    Library Fine complete

--- Algorithms/Implementation/LibraryFine.py
+++ Algorithms/Implementation/LibraryFine.py
@@ -8,7 +8,17 @@ import sys
 
 # Complete the libraryFine function below.
 def libraryFine(d1, m1, y1, d2, m2, y2):
-
+    if y1 > y2:
+        return 10000
+    if y1 < y2:
+        return 0
+    if m1 > m2:
+        return 500 * (m1 - m2)
+    if m1 < m2:
+        return 0
+    if d1 > d2:
+        return 15 * (d1 - d2)
+    return 0
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
