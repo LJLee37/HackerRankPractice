30fb22ec4fc19dc5ed98423f4da55e390faf1b24
LJLee37 <ljlee3759@gmail.com>
2021-01-21 23:04:05 +0900

    Mars Cakewalk complete

---
+++ Algorithms/Greedy/MarcsCakewalk.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the marcsCakewalk function below.
+def marcsCakewalk(calorie):
+    calorie.sort(reverse=True)
+    retval = 0
+    for i in range(len(calorie)):
+        retval += 2 ** i * calorie[i]
+    return retval
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    calorie = list(map(int, input().rstrip().split()))
+
+    result = marcsCakewalk(calorie)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
