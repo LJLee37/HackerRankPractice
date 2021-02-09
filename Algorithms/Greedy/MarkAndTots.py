08e9f8af14d1333930626e27666dbaeab8f1014d
LJLee37 <ljlee3759@gmail.com>
2021-02-10 00:26:08 +0900

    Mark and Toys complete

---
+++ Algorithms/Greedy/MarkAndTots.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k):
+    prices.sort()
+    retval = 0
+    while k > prices[retval]:
+        k -= prices[retval]
+        retval += 1
+    return retval
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
