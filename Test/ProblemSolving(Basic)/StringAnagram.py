e6a6cdb6399ee000b8b70d25b48bf6f8c86f51bd
LJLee37 <ljlee3759@gmail.com>
2021-04-08 21:00:38 +0900

    8th Apr 2021, Certification Test

---
+++ Test/ProblemSolving(Basic)/StringAnagram.py
@@ -0,0 +1,57 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'stringAnagram' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY dictionary
+#  2. STRING_ARRAY query
+#
+
+def stringAnagram(dictionary, query):
+    # Write your code here
+    temp = set(dictionary)
+    counts = {i : dictionary.count(i) for i in temp}
+    retval = []
+    for i in query:
+        if i in temp:
+            retval.append(i)
+        else:
+            retval.append(0)
+    return retval
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    dictionary_count = int(input().strip())
+
+    dictionary = []
+
+    for _ in range(dictionary_count):
+        dictionary_item = input()
+        dictionary.append(str(sorted(dictionary_item)))
+
+    query_count = int(input().strip())
+
+    query = []
+
+    for _ in range(query_count):
+        query_item = input()
+        query.append(str(sorted(query_item)))
+
+    result = stringAnagram(dictionary, query)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
---
+++ Test/ProblemSolving(Basic)/UsernamesChanges.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'possibleChanges' function below.
+#
+# The function is expected to return a STRING_ARRAY.
+# The function accepts STRING_ARRAY usernames as parameter.
+#
+
+def possibleChanges(usernames):
+    # Write your code here
+    retval = []
+    for i in usernames:
+        if sorted(i) == list(i):
+            retval.append("NO")
+        else:
+            retval.append("YES")
+    return retval
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    usernames_count = int(input().strip())
+
+    usernames = []
+
+    for _ in range(usernames_count):
+        usernames_item = input()
+        usernames.append(usernames_item)
+
+    result = possibleChanges(usernames)
+
+    fptr.write('\n'.join(result))
+    fptr.write('\n')
+
+    fptr.close()
+
